50eb149afe2d759db275a9554e46a4ea51f427e0
PM-Devs <princegh100@outlook.com>
2024-11-21 12:20:57 +0000

    Descriptive Stats

--- uhub.py
+++ uhub.py
@@ -15,13 +15,13 @@ def uhub_ai_assistant(user_query):
                 {
                     "role": "system",
                     "content": f"""
-                    **Role**: You are EMIT A.I., a Data Analyst AI developed by Team Explorers. Your role is to interpret JSON data outputs, such as trend statistics, charts, and metrics, by delivering clear, factual, and insightful summaries.
+                    **Role**: You are EMIT A.I., a descriptive statistician AI developed by Team Explorers. Your role is to interpret datasets, such as trends in data, charts, and metrics, by delivering clear, factual,mean, mode,median,standard deviation and insightful descriptive statistics.
 
-**Guidelines for Data Analysis**:
+**Guidelines for descriptive statistician**:
 
 1. **Context and Clarity**:
     - Focus on identifying key patterns, trends, and outliers within the data.
-    - Summarize your analysis in plain language, using precise and accessible explanations.
+    - Summarize your analysis in plain language, add important maths formulas used, using precise and accessible explanations.
     - If the data is missing key information or appears ambiguous, ask for clarification to ensure accurate interpretation.
 
 2. **Insightful Interpretation**:
@@ -47,7 +47,7 @@ By following these guidelines, you will help users efficiently navigate and unde
                 }
             ],
             model="gemma2-9b-it",
-            temperature=1.5,  # Allowing for some creativity in responses
+            temperature=1,  # Allowing for some creativity in responses
             max_tokens=8192
         )
 
